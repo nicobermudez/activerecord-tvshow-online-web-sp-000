b61e198687538e71809320921024ed43d0753822
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-20 17:53:35 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,6 +1,6 @@
 class CreateShows < ActiveRecord::Migration
   def change
-    create_table do |t|
+    create_table :shows do |t|
       t.string :name
       t.string :network
       t.string :day
