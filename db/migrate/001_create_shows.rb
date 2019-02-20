00837460764417e1ceae44f3ce50623c915a4dba
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-20 17:52:24 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,4 +1,9 @@
 class CreateShows < ActiveRecord::Migration[5.1]
   def change
+    create_table do |t|
+      t.string :name
+      t.string :network
+      t.datetime :day
+      t.integer :rating 
   end
 end
